1dfd42aca539c1d360eb620adf8f1e457769963c
ralfzeit <ralfzeit@gmail.com>
2014-03-22 12:11:44 +0700

    Николай Басков

--- iMuseum/iMuseum/Form1.Designer.cs
+++ iMuseum/iMuseum/Form1.Designer.cs
@@ -144,7 +144,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Списать";
+            this.button2.Text = "Spisati";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
