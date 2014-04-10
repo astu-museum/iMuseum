7f5d6f1716df99f888383713ecd00f334bc6d109
ralfzeit <ralfzeit@gmail.com>
2014-04-10 20:40:54 +0700

    Переработан класс Источника получения

--- iMuseum/Sauce.cs
+++ iMuseum/Sauce.cs
@@ -9,7 +9,6 @@ namespace iMuseum
     /// <summary>
     /// Класс,отвечающий  за источник получения экспоната
     /// </summary>
-
     class Sauce
     {
         /// <summary>
@@ -18,7 +17,8 @@ namespace iMuseum
         public Sauce()
         {
             pk_source = 0;
-            address = "";
+            addressValue = "";
+            nameValue = "";
         }
 
         /*
@@ -33,32 +33,70 @@ namespace iMuseum
             get { return pk_sourceValue; }
             set { pk_sourceValue = value; }
         }
-        */
-
-        private int pk_source;
-        public void setPkSource(int id) { this.pk_source = id; }
-        public int getPkSource() { return this.pk_source; }
-
-        private string nameValue;
-
-        /// <summary>
-        /// Наименование Источника
-        /// </summary>
+        
         public string name
         {
             get { return nameValue; }
             set { nameValue = value; }
         }
-
-        private string addressValue;
-
-        /// <summary>
-        /// Адрес Источник
-        /// </summary>
+         
         public string address
         {
             get { return addressValue; }
             set { addressValue = value; }
         }
+         */
+
+        /// <summary>
+        /// Первичный ключ источника получения
+        /// </summary>
+        private int pk_source;
+
+        /// <summary>
+        /// Установить первичный ключ источника получения
+        /// </summary>
+        /// <param name="id">Первичный ключ источника получения</param>
+        public void setPkSource(int id) { this.pk_source = id; }
+
+        /// <summary>
+        /// Вернуть первичный ключ источника получения
+        /// </summary>
+        /// <returns>Первичный ключ источника получения</returns>
+        public int getPkSource() { return this.pk_source; }
+
+        /// <summary>
+        /// Наименование Источника
+        /// </summary>
+        private string nameValue;
+
+        /// <summary>
+        /// Установить наименование источника получения
+        /// </summary>
+        /// <param name="nameVal">Наименование источника получения</param>
+        public void setName(string nameVal) { this.nameValue = nameVal; }
+
+        /// <summary>
+        /// Вернуть наименование источника получения
+        /// </summary>
+        /// <returns>Наименование источника получения</returns>
+        public string getName() { return this.nameValue; }
+        
+
+        /// <summary>
+        /// Адрес источника получения
+        /// </summary>
+        private string addressValue;
+
+        /// <summary>
+        /// Установить адрес источника получения
+        /// </summary>
+        /// <param name="addrVal">Адрес источника получения</param>
+        public void setAddress(string addrVal) { this.addressValue = addrVal; }
+
+        /// <summary>
+        /// Вернуть адрес источника получения
+        /// </summary>
+        /// <returns>Адрес источника получения</returns>
+        public string getAddress() { return this.addressValue; }
     }
 }
