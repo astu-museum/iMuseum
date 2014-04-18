ac9ae63377e519a192d5457f8cf3413c709292e4
gatereverser <gatereverser@gmail.com>
2014-04-18 12:13:32 +0700

    РАБОТАЮЩИЙ ПОИСК ПО ВЫСТАВКЕ,НО РЕБЯТА ЭТО Ж ОПАСНОСТЬ.ПОТЕСТИТЕ КТО-НИБДУЬ ПО ХАРДКОРУ!

--- iMuseum/AddExpExb.cs
+++ iMuseum/AddExpExb.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,6 +19,10 @@ namespace iMuseum
         List<Exponat> saveExp;
         List<int> saveCheck;
 
+        //СЕКРЕТНЫЙ ЧЕКО САВЕР
+        Hashtable mapPk = new Hashtable();
+
+
         public AddExpExb()
         {
             InitializeComponent();
@@ -25,18 +30,87 @@ namespace iMuseum
 
         private void search(object sender, EventArgs e)
         {
+
+            
+
+            Check_Load();
+
+
   
             User.exponats = new List<Exponat>(saveExp);
-            User.Checks = new List<int>(saveCheck);
+          //  User.Checks = new List<int>(saveCheck);
 
+         
 
             sourceSettings srccc = new sourceSettings();
             srccc.ShowDialog();
 
            User.find_exponats_exhibition();
-           Load_some();
+           //Load_some();
+           Load_Other();
         }
 
+
+
+        //СУПЕР ЗАГРУЗКА ЧЕКОВ
+        void Check_Load()
+        {
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+
+                //  MessageBox.Show(User.exponats[i].getPkExponat().ToString());
+
+                if (dataGridView1.Rows[i].Cells[0].Value != null && (bool)dataGridView1.Rows[i].Cells[0].Value)
+                {
+                    mapPk[User.exponats[i].getPkExponat()] = true;
+                    //  MessageBox.Show(User.exponats[i].getPkExponat().ToString());
+                }
+                else
+                {
+                    mapPk[User.exponats[i].getPkExponat()] = false;
+
+                }
+            }
+        }
+
+        //Загрузка после поиска
+        private void Load_Other()
+        {
+            title.DataPropertyName = "name";
+            mesto.DataPropertyName = "placeStr";
+            damage.DataPropertyName = "damageStr";
+            price.DataPropertyName = "price";
+
+
+            a.DataPropertyName = "inumber";
+            b.DataPropertyName = "sourceValue";
+            c.DataPropertyName = "date";
+            d.DataPropertyName = "typeSobStr";
+
+
+            dataGridView1.DataSource = User.exponats;//User.exponats;
+
+
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+
+                dataGridView1.Rows[i].Cells[0].Value = false;
+
+            }
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+
+                
+
+                    dataGridView1.Rows[i].Cells[0].Value = mapPk[User.exponats[i].getPkExponat()];
+          
+
+            }
+
+
+        }
      
 
         private void Load_some()
@@ -76,14 +150,24 @@ namespace iMuseum
             
             for (int i = 0; i < dataGridView1.Rows.Count; i++)
             {
-               // MessageBox.Show("dafaq");
+             
                 dataGridView1.Rows[i].Cells[0].Value = false;
+
+
+                //Внулим все чеки
+                mapPk.Add(User.exponats[i].getPkExponat(), false);
             }
   
             for (int i = 0; i < User.Checks.Count; i++)
             {
-              //  MessageBox.Show("maniga");
+            
+
                 dataGridView1.Rows[User.Checks[i]].Cells[0].Value = true;
+
+
+                //ПРоставим некоторые чеки
+
+              //  mapPk[User.exponats[User.Checks[i]].getPkExponat()] = true;
             }
 
 
@@ -101,12 +185,14 @@ namespace iMuseum
             saveExp = new List<Exponat>(User.exponats);
             saveCheck = new List<int>(User.Checks);
 
+            //НАДО ПОМНИТЬ ВСЕ ЧЕКИ ПО ПК ЭКСПОНАТА
+
             Load_some();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < dataGridView1.Rows.Count;i++ )
+           /* for (int i = 0; i < dataGridView1.Rows.Count;i++ )
             {
 
                 if (dataGridView1.Rows[i].Cells[0].Value != null && (bool)dataGridView1.Rows[i].Cells[0].Value)
@@ -116,9 +202,25 @@ namespace iMuseum
                     User.exhibitions[0].addExponat(User.exponats[i].getPkExponat());
                   //  MessageBox.Show(User.exponats[i].name);
                 }
+            }*/
+
+            Check_Load();
+
+
+
+            foreach (DictionaryEntry de in mapPk)
+            {
+                if((bool)de.Value){
+                    User.exhibitions[0].addExponat((int)de.Key);
+                }
+
+           //     Console.WriteLine("Key = {0}, Value = {1}", de.Key, de.Value);
             }
 
-            User.exhibitions[0].save();
+
+
+
+                User.exhibitions[0].save();
             this.Close();
         }
     }
